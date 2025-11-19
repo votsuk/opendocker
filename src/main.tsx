52f487b911c2b7914364b27a32b6f08bc7182d20
Steven <steven@Steve.local>
2025-11-19 11:04:01 -0500

    feat: Update console debugger keybind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 #!/usr/bin/env bun
 
-import { useRenderer, render, useKeyboard } from "@opentui/react";
+import { createRoot, useKeyboard, useRenderer } from "@opentui/react";
+import { createCliRenderer } from "@opentui/core";
 import ContainersPane from "./components/ContainersPane";
 import LogsPane from "./components/LogsPane";
-import ImagesPane from "./components/ImagesPane";
-import VolumesPane from "./components/VolumesPanes";
 import BaseLayout from "./layouts/BaseLayout";
 
 function App() {
@@ -15,7 +14,7 @@ function App() {
             process.exit(0);
         }
 
-        if (key.ctrl && key.name === "k") {
+        if (key.ctrl && key.name === "d") {
             renderer?.console.toggle()
             renderer?.toggleDebugOverlay()
         }
@@ -29,12 +28,16 @@ function App() {
                 gap={1}
             >
                 <ContainersPane />
-                <ImagesPane />
-                <VolumesPane />
             </box>
             <LogsPane />
         </BaseLayout>
     )
 }
 
-render (<App />);
+async function main() {
+    const renderer = await createCliRenderer();
+  const root = createRoot(renderer);
+  root.render(<App />);
+}
+
+main();
